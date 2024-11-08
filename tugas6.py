3739946e26c8d2854058d46aa0763fa193292c24
Naddnuutt <nadiawidianti06@gmail.com>
2024-11-08 09:40:22 +0800

    Add files via upload

---
+++ tugas6.py
@@ -0,0 +1,25 @@
+# variabel assigment value
+x = 20
+y = 29
+print("nilai dari x adalah :", y)
+
+# aturan penanaman variabel
+luas = 4 # bisa berupa kata
+bernadia_sad = 2006 # jika dua kata, digabung dg "_"
+bernadia = 6 # boleh gabungan capital non-kapital
+Bernadia = 5
+bernadia20 = 26 # jika menggabungkan huruf dan angka, 
+print("nilai dari bernadia adalah:", bernadia)
+
+
+a = 2 # integer bilangan bulat
+print("tipe data dari a:", type (a))
+
+b = 4.6 # float bilangan pecahan
+print("tipe data dari b :", type (b))
+
+c = "gacor" # string, gabungan karakter
+print("tipe data dari c :", type (c))
+
+d = False # Boolean, true false
+print("tipe data dari d :", type (d))
