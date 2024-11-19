2039d06bee6174332b036c458fc8343719aa6d99
Naddnuutt <nadiawidianti06@gmail.com>
2024-11-18 20:19:47 -0800

    Create tugas7.py

---
+++ tugas7.py
@@ -0,0 +1,9 @@
+# Menghitung luas persegi 
+# nama : Ni Wayan Nadia Widianti
+
+print(" Menghitung Persegi ")
+alas = int(input("Masukka panjang : "))
+tinggi = int(input("Masukan lebar : "))
+
+hasil = panjang * tinggi 
+print(f"Hasil dari {panjang} x {lebar} adalah : {hasil}")
